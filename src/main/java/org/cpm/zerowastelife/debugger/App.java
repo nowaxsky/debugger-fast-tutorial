b3cdbc26507954a385d6d76170b1944d4cf1c46d
CPM <cpm.chuck@gmail.com>
2017-08-06 22:52:41 +0800

    update

--- src/main/java/org/cpm/zerowastelife/debugger/App.java
+++ src/main/java/org/cpm/zerowastelife/debugger/App.java
@@ -1,13 +1,16 @@
 package org.cpm.zerowastelife.debugger;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class App {
+	
+    public static void main( String[] args ) {
+    	
+    	Employee em25 = new Employee(25, "Joy", 80000);
+    	System.out.println(em25.getId());
+    	System.out.println(em25.getName());
+    	System.out.println(em25.getSalary());
+    	em25.promote(5000);
+    	System.out.println(em25.getSalary());
+    	
+    	
     }
 }
