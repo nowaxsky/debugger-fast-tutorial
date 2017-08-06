800085db65fa126b4d6ae127fde5f5a4c948813d
CPM <cpm.chuck@gmail.com>
2017-08-06 22:51:38 +0800

    building the project

---
+++ src/main/java/org/cpm/zerowastelife/debugger/App.java
@@ -0,0 +1,13 @@
+package org.cpm.zerowastelife.debugger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
